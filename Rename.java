4e3748c54515ec950c8e04e23899c20f1d21be2c
rajeshct <rajeshchandratiwari@gmail.com>
2015-06-23 00:06:30 +0530

    Update Rename.java

--- Rename.java
+++ Rename.java
@@ -1,32 +1,23 @@
 import java.io.File;
+
+
 public class Check {
 public static void main(String[] ar)
-{File f=new File("ab.txt");
+{File temporay=new File("ab.txt");
 int l=0;
-String s=f.getAbsolutePath().replace("\\ab.txt","");
-String t=s.substring((s.lastIndexOf("\\")+1));
-s+="\\";
-File f1=new File(s);
-String[] st=f1.list();
-
-for(int i=0;i<st.length;i++)
-{if(st[i].endsWith(".png"))
-	{l++;
-     File fz=new File(st[i]);
-	 File gh=new File(t+l+".png");
+String getPath=temporay.getAbsolutePath().replace("\\ab.txt","");
+String directoryName=getPath.substring((getPath.lastIndexOf("\\")+1));
+getPath+="\\";
+File f1=new File(getPath);
+String[] getAllFile=f1.list();  //get all file in current directory
+for(int i=0;i<getAllFile.length;i++)
+{    String type=getAllFile[i].substring(getAllFile[i].lastIndexOf("."),getAllFile[i].length());
+	 l++;
+     File fz=new File(getAllFile[i]);
+	 File gh=new File(new StringBuffer(directoryName).append(l).append(type).toString());
 	 fz.renameTo(gh);
-	}
-else if(st[i].endsWith(".jpg"))
-{l++;
-File fz=new File(st[i]);
-File gh=new File(t+l+".jpg");
-fz.renameTo(gh);}
-else
-{
-	l++;
-    File fz=new File(st[i]);
-	 File gh=new File(t+l+".jpeg");
-	 fz.renameTo(gh);}
 }
 }
 }
+
+
